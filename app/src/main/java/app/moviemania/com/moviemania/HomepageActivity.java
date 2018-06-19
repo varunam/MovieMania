0a9886a6f2702adce42b9270a7027a14c882c26a
varunvgnc <Travel@12345>
2018-06-19 11:13:47 +0530

    implemented suggestions 1. RecyclerView is now restoring state after configuration change 2. Coming back from favourites is now taking back to favourites. 3. Internet connection is now being checked every time we try to load data from network.

--- app/src/main/java/app/moviemania/com/moviemania/HomepageActivity.java
+++ app/src/main/java/app/moviemania/com/moviemania/HomepageActivity.java
@@ -15,7 +15,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -37,9 +39,10 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
     private static String URL_KEY = "url";
     private static final String TAG = "HomePageActivity";
     private static final String MOST_POPULAR_STRING = "Most Popular";
-    private static final String MOST_RATED_STRING = "Most Rated";
+    private static final String TOP_RATED_STRING = "Most Rated";
     private static final String FAVOURITES_STRING = "Favourites";
     private static final String CHOSEN_OPTION_KEY = "chosen";
+    private static final String SCROLL_POSITION = "scroll position";
     private static final String SELECT_FILTER = "Select Filter";
 
     public static final String REQUEST_GET = "GET";
@@ -48,7 +51,7 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
 
     //API URLs
     public static final String BASE_URL = "http://api.themoviedb.org/3";
-    public static final String API_KEY = "?api_key=211742f7f301a9352fcd87caf053db24";
+    public static final String API_KEY = "?api_key=" + BuildConfig.THE_GUARDIAN_API_KEY;
     private static final String MODE_MOST_POPULAR = "/movie/popular";
     private static final String MODE_TOP_RATED = "/movie/top_rated";
     private static final String MOST_POPULAR = BASE_URL + MODE_MOST_POPULAR + API_KEY;
@@ -72,7 +75,8 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
     private List<Movie> movieList;
     private android.support.v4.app.LoaderManager loaderManager;
     private Loader<String> loader;
-    private AppExecutors appExecutors;
+    private GridLayoutManager gridLayoutManager;
+    private int position = 0;
 
     private boolean option_popular = false, option_rated = false, option_favourite = false;
 
@@ -81,51 +85,82 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        Bundle loaderBundle = new Bundle();
         loaderManager = getSupportLoaderManager();
-        loaderBundle.putString(URL_KEY, MOST_POPULAR);
         loader = loaderManager.getLoader(LOADER_ID);
         progressBar = new ProgressDialog(this);
         recyclerView = findViewById(R.id.recyclerViewID);
-        appExecutors = AppExecutors.getInstance(getApplicationContext());
         movieList = new ArrayList<>();
         recyclerViewAdapter = new RecyclerViewAdapter(this, movieList);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        int posterWidth = 350;
+        gridLayoutManager = new GridLayoutManager(this, calculateBestSpanCount(posterWidth));
+        recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setAdapter(recyclerViewAdapter);
 
-        if (!networkUnavailable())
-            showNoNetworkDialog();
-        else if (savedInstanceState != null) {
+        if (savedInstanceState != null) {
             String chosen_option = savedInstanceState.getString(CHOSEN_OPTION_KEY);
+            position = savedInstanceState.getInt(SCROLL_POSITION);
             if (chosen_option != null)
                 switch (chosen_option) {
                     case MOST_POPULAR_STRING:
-                        loaderBundle.putString(URL_KEY, MOST_POPULAR);
-                        setChosenOption(MOST_POPULAR_STRING);
-                        loadData(loaderBundle);
+                        loadData(MOST_POPULAR);
                         break;
-                    case MOST_RATED_STRING:
-                        loaderBundle.putString(URL_KEY, TOP_RATED);
-                        setChosenOption(MOST_RATED_STRING);
-                        loadData(loaderBundle);
+                    case TOP_RATED_STRING:
+                        loadData(TOP_RATED);
                         break;
                     case FAVOURITES_STRING:
                         loadFavouriteMovies();
+                        break;
+                    default:
+                        break;
                 }
         } else {
-            loadData(loaderBundle);
+            loadData(MOST_POPULAR);
         }
     }
 
-    private void loadData(Bundle loaderBundle) {
-        if (loader == null)
+    private int calculateBestSpanCount(int posterWidth) {
+        Display display = getWindowManager().getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+        float screenWidth = outMetrics.widthPixels;
+        return Math.round(screenWidth / posterWidth);
+    }
+
+    private void loadData(String category) {
+        if (!networkUnavailable()) {
+            showNoNetworkDialog(category);
+            return;
+        }
+        if (movieList != null)
+            movieList.clear();
+
+        recyclerView.smoothScrollToPosition(position);
+        Bundle loaderBundle = new Bundle();
+        loaderBundle.putString(URL_KEY, category);
+
+        if (category.equals(MOST_POPULAR))
+            setChosenOption(MOST_POPULAR_STRING);
+        else if (category.equals(TOP_RATED))
+            setChosenOption(TOP_RATED_STRING);
+
+        if (loader == null) {
+            Log.e(TAG, "initiating loader to load " + getChosenOption() + " movies...");
+            //If I don't destroy loader, details are not loaded when option is chosen from filter. Need help here
+            //use case below
+            //1. comment below line and run the app
+            //2. MOST POPULAR movies will be loaded. now use filter to change it to top rated
+            //3. It won't load. Try same uncommenting below line, it works. Please explain
+            loaderManager.destroyLoader(LOADER_ID);
             loaderManager.initLoader(LOADER_ID, loaderBundle, this);
-        else
+        } else {
+            Log.e(TAG, "restarting loader to load " + getChosenOption() + " movies...");
             loaderManager.restartLoader(LOADER_ID, loaderBundle, this);
+        }
     }
 
     private void loadFavouriteMovies() {
         MainViewModel mainViewModel = ViewModelProviders.of(HomepageActivity.this).get(MainViewModel.class);
+        Log.e(TAG, "Extracted mainViewModel...");
         mainViewModel.getMovies().observe(HomepageActivity.this, new Observer<List<Movie>>() {
             @Override
             public void onChanged(@Nullable List<Movie> movies) {
@@ -137,17 +172,25 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
         setChosenOption(FAVOURITES_STRING);
     }
 
-    private void showNoNetworkDialog() {
+    private void showNoNetworkDialog(final String category) {
         new AlertDialog.Builder(this)
                 .setTitle("Uh-oh!")
                 .setMessage("Seems like you don't have network connection.")
-                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                .setNegativeButton("Retry", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        finish();
+                        loadData(category);
                     }
                 })
+                .setCancelable(false)
                 .create().show();
+    }
+
+    @Override
+    protected void onStop() {
+        if (getChosenOption().equals(FAVOURITES_STRING))
+            getSupportLoaderManager().destroyLoader(LOADER_ID);
+        super.onStop();
     }
 
     private boolean networkUnavailable() {
@@ -170,22 +213,13 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
                     .setPositiveButton(MOST_POPULAR_STRING, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            setChosenOption(MOST_POPULAR_STRING);
-                            if (movieList != null)
-                                movieList.clear();
-                            Bundle loaderBundle = new Bundle();
-                            loaderBundle.putString(URL_KEY, MOST_POPULAR);
-                            loadData(loaderBundle);
+                            loadData(MOST_POPULAR);
                         }
                     })
-                    .setNegativeButton(MOST_RATED_STRING, new DialogInterface.OnClickListener() {
+                    .setNegativeButton(TOP_RATED_STRING, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            setChosenOption(MOST_RATED_STRING);
-                            movieList.clear();
-                            Bundle loaderBundle = new Bundle();
-                            loaderBundle.putString(URL_KEY, TOP_RATED);
-                            loadData(loaderBundle);
+                            loadData(TOP_RATED);
                         }
                     })
                     .setCancelable(false)
@@ -203,7 +237,7 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
     private String getChosenOption() {
         String result;
         if (option_rated)
-            result = MOST_RATED_STRING;
+            result = TOP_RATED_STRING;
         else if (option_favourite)
             result = FAVOURITES_STRING;
         else
@@ -220,7 +254,7 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
                 option_favourite = false;
                 option_rated = false;
                 break;
-            case MOST_RATED_STRING:
+            case TOP_RATED_STRING:
                 option_popular = false;
                 option_favourite = false;
                 option_rated = true;
@@ -236,6 +270,8 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
     @Override
     public void onSaveInstanceState(Bundle outState) {
         outState.putString(CHOSEN_OPTION_KEY, getChosenOption());
+        outState.putInt(SCROLL_POSITION, gridLayoutManager.findFirstCompletelyVisibleItemPosition());
+        Log.e(TAG, "Saving instance with option " + getChosenOption());
         super.onSaveInstanceState(outState);
     }
 
@@ -263,7 +299,7 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
                             result.append(buffer);
 
                         bufferedReader.close();
-                        Log.e(TAG, "Retrieved Json: \n" + result.toString());
+                        //Log.e(TAG, "Retrieved Json: \n" + result.toString());
 
                         return result.toString();
                     } else
@@ -312,7 +348,7 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
                 String language = movieObject.getString(LANGUAGE);
                 int id = movieObject.getInt(ID);
 
-                Log.e(TAG, "Retrieved Details:\n" +
+                /*Log.e(TAG, "Retrieved Details:\n" +
                         TITLE + " " + title + "\n" +
                         OVERVIEW + " " + overview + "\n" +
                         VOTE_COUNT + " " + votes + "\n" +
@@ -320,7 +356,7 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
                         POSTER_PATH + " " + poster_path + "\n" +
                         ADULT + " " + adult + "\n" +
                         RELEASE_DATE + " " + released_date + "\n" +
-                        ID + " " + id + "\n");
+                        ID + " " + id + "\n");*/
 
                 Movie movie = new Movie();
                 movie.setTitle(title);
@@ -334,9 +370,9 @@ public class HomepageActivity extends AppCompatActivity implements LoaderManager
                 movie.setId(id);
 
                 movieList.add(movie);
-                recyclerViewAdapter.setMovieList(movieList);
-                recyclerViewAdapter.notifyDataSetChanged();
             }
+            recyclerViewAdapter.setMovieList(movieList);
+            recyclerViewAdapter.notifyDataSetChanged();
         } catch (JSONException e) {
             Log.e(TAG, "Unable to parse result to JSON Object " + e.toString());
         }
